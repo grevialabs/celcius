fc8e42f078ae50db53e32b435e1dad783973e623
Rusdi Karsandi <rusdi.karsandi@kawanlamaretail.com>
2018-12-11 16:11:55 +0700

    remove localhost uri path for dynamic route

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<script src="http://localhost/celcius/cell/cell.js"></script>
-<script src="http://localhost/celcius/content/home.js"></script>
+<script src="cell/cell.js"></script>
+<script src="content/home.js"></script>
 <body>
 <div id="blood"></div>
 </body>
